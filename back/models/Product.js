d5a69357775ca18b6155c4109d70a91dd0a95086
rebolloso mathieu <mathieurebolloso@gmail.com>
2022-03-19 23:30:12 +0100

    changes

--- back/models/Product.js
+++ back/models/Product.js
@@ -80,7 +80,7 @@ exports.find = () => {
 exports.findById = (id) => {
   return new Promise((resolve, reject) =>
     resolve(JSON.parse(JSON.stringify(products)).find(product =>
-      product._id == id)
+      product._id === id)
     )
   );
 }
